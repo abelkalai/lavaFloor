2b8e62f143e3f6448d633fabab89acc4f558ecd1
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-02-29 00:21:01 -0500

    Removed cors, changed file paths

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express= require('express');
 const app= express()
 const path = require('path')
-const cors = require('cors')
 
-app.use(cors())
 app.use('/',express.static(__dirname+'/'))
 
 app.get('/',function(response){

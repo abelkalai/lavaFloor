856b1ee899b235a456143e41fc9b70e3f103266c
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-03-22 23:04:22 -0400

    Removed watch on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: './static/lib/game.js',
-  watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'

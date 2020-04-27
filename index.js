fff54f93c5ad7367325e8b342228e9bea39d0d94
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-04-27 19:44:07 -0400

    Removed pathing to gameObjects from express app

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
-
-app.use("/gameObjects", express.static(__dirname + "/./src/lib/gameObjects"));
 app.use('/',express.static(__dirname+'/'))
 app.get('/',function(response){
     response.sendFile(path.join(__dirname+'/index.html'))

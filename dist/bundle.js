8c6b68bb98dfe3cc4e76746b8ee7cc3494b04e31
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-04-27 20:06:53 -0400

    Added path shortcuts to preload assets

--- dist/bundle.js
+++ dist/bundle.js
@@ -1312,47 +1312,47 @@ class Preload extends Phaser.Scene {
       loadText.destroy();
     });
     
-    //Load Enviornment Assetss
-    this.load.image("background", "/src/assets/env/background.png");
-    this.load.image("platform", "/src/assets/env/grass.png");
-    this.load.image("platformSmall", "/src/assets/env/grassSmall.png");
-    this.load.image("lava", "/src/assets/env/lava.png");
-    this.load.image("wall", "/src/assets/env/invisibleWall.png");
-    this.load.image("cloud", "/src/assets/env/cloud.png");
-    this.load.image("star", "/src/assets/env/star.png");
+    //Load Enviornment Assets
+    this.load.image("background", "env/background.png");
+    this.load.image("platform", "env/grass.png");
+    this.load.image("platformSmall", "env/grassSmall.png");
+    this.load.image("lava", "env/lava.png");
+    this.load.image("wall", "env/invisibleWall.png");
+    this.load.image("cloud", "env/cloud.png");
+    this.load.image("star", "env/star.png");
 
     // Health
-    this.load.image("heart_3", "/src/assets/sprites/heartThree.png");
-    this.load.image("heart_2", "/src/assets/sprites/heartTwo.png");
-    this.load.image("heart_1", "/src/assets/sprites/heartOne.png");
-    this.load.image("heart_0", "/src/assets/sprites/heartZero.png");
+    this.load.image("heart_3", "sprites/heartThree.png");
+    this.load.image("heart_2", "sprites/heartTwo.png");
+    this.load.image("heart_1", "sprites/heartOne.png");
+    this.load.image("heart_0", "sprites/heartZero.png");
 
     // Heart to Pick Up
-    this.load.spritesheet("heart", "/src/assets/sprites/heartInGame.png", {
+    this.load.spritesheet("heart", "sprites/heartInGame.png", {
       frameWidth: 50,
       frameHeight: 20
     });
 
     // Coin
-    this.load.spritesheet("coin", "/src/assets/sprites/coin.png", {
+    this.load.spritesheet("coin", "sprites/coin.png", {
       frameWidth: 563,
       frameHeight: 564
     });
 
     // Ground Enemy
-    this.load.spritesheet("enemyOne", "/src/assets/sprites/enemyOne.png", {
+    this.load.spritesheet("enemyOne", "sprites/enemyOne.png", {
       frameWidth: 350,
       frameHeight: 371
     });
 
     // Ground Enemy 2
-    this.load.spritesheet("enemyTwo", "/src/assets/sprites/enemyTwo.png", {
+    this.load.spritesheet("enemyTwo", "sprites/enemyTwo.png", {
       frameWidth: 580,
       frameHeight: 650
     });
 
     // Enemy 3
-    this.load.spritesheet("enemyThree", "/src/assets/sprites/enemyThree.png", {
+    this.load.spritesheet("enemyThree", "sprites/enemyThree.png", {
       frameWidth: 60,
       frameHeight: 48
     });
@@ -1360,7 +1360,7 @@ class Preload extends Phaser.Scene {
     // Invincibility Potion
     this.load.spritesheet(
       "iPotion",
-      "/src/assets/sprites/invincibilityPotion.png",
+      "sprites/invincibilityPotion.png",
       {
         frameWidth: 125,
         frameHeight: 120
@@ -1370,7 +1370,7 @@ class Preload extends Phaser.Scene {
     // Score Multiplier Potion
     this.load.spritesheet(
       "mPotion",
-      "/src/assets/sprites/multiplierPotion.png",
+      "sprites/multiplierPotion.png",
       {
         frameWidth: 125,
         frameHeight: 120
@@ -1378,66 +1378,66 @@ class Preload extends Phaser.Scene {
     );
 
     // Player
-    this.load.spritesheet("character", "/src/assets/sprites/stand.png", {
+    this.load.spritesheet("character", "sprites/stand.png", {
       frameWidth: 67,
       frameHeight: 92
     });
 
-    this.load.spritesheet("character_b", "/src/assets/sprites/standBackFacing.png", {
+    this.load.spritesheet("character_b", "sprites/standBackFacing.png", {
       frameWidth: 67,
       frameHeight: 92
     });
-    this.load.spritesheet("walk", "/src/assets/sprites/walk.png", {
+    this.load.spritesheet("walk", "sprites/walk.png", {
       frameWidth: 80,
       frameHeight: 92
     });
-    this.load.spritesheet("walk_b", "/src/assets/sprites/walkBackFacing.png", {
+    this.load.spritesheet("walk_b", "sprites/walkBackFacing.png", {
       frameWidth: 80,
       frameHeight: 92
     });
-    this.load.spritesheet("jump", "/src/assets/sprites/jump.png", {
+    this.load.spritesheet("jump", "sprites/jump.png", {
       frameWidth: 80,
       frameHeight: 96
     });
-    this.load.spritesheet("jump_b", "/src/assets/sprites/jumpBackFacing.png", {
+    this.load.spritesheet("jump_b", "sprites/jumpBackFacing.png", {
       frameWidth: 80,
       frameHeight: 96
     });
 
     // Pause Button and sound
-    this.load.image("pauseImage", "/src/assets/env/pause.png");
-    this.load.audio("pauseSound", "/src/assets/sounds/pause.mp3");
+    this.load.image("pauseImage", "env/pause.png");
+    this.load.audio("pauseSound", "sounds/pause.mp3");
 
     // Main Scene Sounds
-    this.load.audio("collectCoin", "/src/assets/sounds/coinCollect.mp3");
-    this.load.audio("collectHeart", "/src/assets/sounds/getHeart.mp3");
+    this.load.audio("collectCoin", "sounds/coinCollect.mp3");
+    this.load.audio("collectHeart", "sounds/getHeart.mp3");
     this.load.audio(
       "coinMultiplierCollect",
-      "/src/assets/sounds/coinMultiplierCollect.mp3"
+      "sounds/coinMultiplierCollect.mp3"
     );
     this.load.audio(
       "multiplierPotion",
-      "/src/assets/sounds/multiplierPotion.mp3"
+      "sounds/multiplierPotion.mp3"
     );
-    this.load.audio("collideEnemy", "/src/assets/sounds/collideEnemy.mp3");
-    this.load.audio("scoreIncrease", "/src/assets/sounds/heightIncrease.mp3");
+    this.load.audio("collideEnemy", "sounds/collideEnemy.mp3");
+    this.load.audio("scoreIncrease", "sounds/heightIncrease.mp3");
 
     //Death Sound
-    this.load.audio("deathSound", "/src/assets/sounds/deathSound.mp3");
+    this.load.audio("deathSound", "sounds/deathSound.mp3");
 
     // Background Music
     this.load.audio(
       "backgroundMusic",
-      "/src/assets/music/backgroundMusic.wav"
+      "music/backgroundMusic.wav"
     );
     this.load.audio(
       "invincibleMusic",
-      "/src/assets/music/invincibleMusic.wav"
+      "music/invincibleMusic.wav"
     );
 
     // Lava Sound Effects
-    this.load.audio("lava", "/src/assets/sounds/lavaApproaching.mp3");
-    this.load.audio("burn", "/src/assets/sounds/burn.mp3");
+    this.load.audio("lava", "sounds/lavaApproaching.mp3");
+    this.load.audio("burn", "sounds/burn.mp3");
 
   }
 
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
+/* Enable Path Shortcuts */
+app.use('/env',express.static(__dirname+'/src/assets/env'))
+app.use('/logos',express.static(__dirname+'/src/assets/logos'))
+app.use('/music',express.static(__dirname+'/src/assets/music'))
+app.use('/sounds',express.static(__dirname+'/src/assets/sprites'))
+app.use('/stylesheets',express.static(__dirname+'/src/assets/stylesheets'))
+
 app.use('/',express.static(__dirname+'/'))
 app.get('/',function(response){
     response.sendFile(path.join(__dirname+'/index.html'))

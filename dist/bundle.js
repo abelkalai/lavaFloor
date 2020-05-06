e42965db499413a1963fff8969fb9e58e5253bad
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-05-06 18:02:56 -0400

    Removed github and linkedin logos, added them to title screen

--- dist/bundle.js
+++ dist/bundle.js
@@ -1228,17 +1228,29 @@ class Menu extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, "background");
-    this.add.text(207.5, 50, "The Floor is Lava!", {
+    this.add.text(7.5, 7.5, "github.com/abelkalai", {
       fontFamily: "inGame",
-      fontSize: "60px",
-      color: "#000000"
+      fontSize: "26px",
+      color: "#000000",
+    });
+    this.add.text(7.5, 30, "linkedin.com/in/abelkalai/", {
+      fontFamily: "inGame",
+      fontSize: "26px",
+      color: "#000000",
     });
 
-    this.add.text(185, 400, "Press Enter to Start", {
+    this.add.text(207.5, 65, "The Floor is Lava!", {
       fontFamily: "inGame",
       fontSize: "60px",
-      color: "#000000"
+      color: "#000000",
     });
+
+    this.add.text(185, 450, "Press Enter to Start", {
+      fontFamily: "inGame",
+      fontSize: "60px",
+      color: "#000000",
+    });
+
     this.enterKey = this.input.keyboard.addKey("ENTER");
   }
 
